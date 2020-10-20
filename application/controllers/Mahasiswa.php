e7954435c4e0724ef2ab409b36df8b3e0d1231d8
unknown <andriancimen123@gmail.com>
2020-10-20 12:51:29 +0700

    membuat class latihan

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,11 @@
+<?php
+
+class Mahasiswa extends CI_Controller
+{
+
+
+  public function pertemuan2()
+  {
+    echo 'Dosen mengajarkan github';
+  }
+}
---
+++ application/controllers/latihan.php
@@ -0,0 +1,9 @@
+<?php
+class latihan extends CI_Controller
+{
+  public function pertama()
+  {
+    echo 'cetak dari controller';
+    echo 'andriano';
+  }
+}
