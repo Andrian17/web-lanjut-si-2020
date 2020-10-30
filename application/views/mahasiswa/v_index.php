32ca542ee9a930836c909b7e5e2fd51afe0842e3
unknown <andriancimen123@gmail.com>
2020-10-30 18:57:40 +0700

    membuat data diri menggunakan bootstrap

--- application/views/mahasiswa/v_index.php
+++ application/views/mahasiswa/v_index.php
@@ -5,45 +5,91 @@
 	<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
 </head>
 <body>
-
-	<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link" href="https://www.instagram.com/andriancimen/" target="_blank"><img src="<?php echo base_url('assets/img/instagram.PNG') ?>" width="30px"></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="https://www.instagram.com/andriancimen/" target="_blank"><img src="<?php echo base_url('assets/img/facebook.PNG') ?>" width="30px"></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="https://www.instagram.com/andriancimen/" target="_blank"><img src="<?php echo base_url('assets/img/twitter.PNG') ?>" width="30px"></a>
-  </li>
-</ul>
-
-
-	<h2>Data Mahasiswa</h2>
+	<!-- <h2>Data Mahasiswa</h2>
 	<p>Nama	:	<?php echo $nama; ?></p>
 	<p>Umur	:	<?php echo $umur; ?></p>
 	<p>Nim	:	<?php echo $nim; ?></p>
-	<p>Jurusan	:	<?php echo $jurusan; ?></p>
+	<p>Jurusan	:	<?php echo $jurusan; ?></p> -->
 
 	<div class="container">
-		<table class="table table-bordered">
-		<tr>
-			<td>Nama</td>
-			<td>Nim</td>
-			<td>Jurusan</td>
-		</tr>
-		<tr>
-			<td>Andrian</td>
-			<td>1901050024</td>
-			<td>D3 Sistem Informasi</td>
-		</tr>
-	</table>
+
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">Tugas</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Tambah Data</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Andrian</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<div class="alert alert-primary" role="alert" align="center">
+			Andrian/1901050024
+		</div>
+
+
+		<div class="card">
+			<div class="card-header">
+				Data Mahasiswa
+			</div>
+			<div class="card-body">
+
+				<table class="table table-bordered">
+					<tr>
+						<td>Nomor</td>
+						<td>Nama</td>
+						<td>Nim</td>
+						<td>Alamat</td>
+						<td>Aksi</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Andrian</td>
+						<td>1901050024</td>
+						<td>Jatibaru</td>
+						<td>
+							<button type="button" class="btn btn-warning">Edit</button> 
+							<button type="button" class="btn btn-danger">Delete</button>
+						</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Ahmad</td>
+						<td>1901050099</td>
+						<td>Rasalewi</td>
+						<td>
+							<button type="button" class="btn btn-warning">Edit</button> 
+							<button type="button" class="btn btn-danger">Delete</button>
+						</td>
+					</tr>
+				</table>
+
+			</div>
+			<div class="card-footer text-muted">
+				<ul class="nav justify-content-center">
+					<li class="nav-item">
+						<a class="nav-link" href="https://www.instagram.com/andriancimen/" target="_blank"><img src="<?php echo base_url('assets/img/instagram.PNG') ?>" width="30px"></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="https://www.instagram.com/andriancimen/" target="_blank"><img src="<?php echo base_url('assets/img/facebook.PNG') ?>" width="30px"></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="https://www.instagram.com/andriancimen/" target="_blank"><img src="<?php echo base_url('assets/img/twitter.PNG') ?>" width="30px"></a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
-
-	
-
-	
-
 
 	
 </body>
